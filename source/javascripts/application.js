60f04843abb913ebbac70f59ee830ca0712b817b
Robin Handous <robin.handous@gmail.com>
2018-05-02 15:08:27 +0200

    home design + navbar

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,10 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+document.querySelector('.home').scrollIntoView({
+  behavior: 'smooth'
+});
+document.querySelector('.music').scrollIntoView({
+  behavior: 'smooth'
+});
+
